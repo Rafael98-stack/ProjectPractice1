824fc3b2e33b66e6b958456727073b41fd3a24f4
Rafael98-stack <rafaelcasapaojr@gmail.com>
2024-09-11 11:56:15 +0200

    Creating Entities

---
+++ ProjectPractice1/Entities/Persona.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectPractice1.Entities
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthdate { get; set; }
+        public int height { get; set; }
+
+
+    }
+}
